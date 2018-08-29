b8dd8217a62972a2c6dbc3b8f26c69a64068a289
CharlesLiu02 <roomcool02@gmail.com>
2018-08-29 08:49:45 -0700

    Added countdowntimer to switch to turn off after 5 seconds. Switching off early cancels the timer.

--- app/src/main/java/com/example/uselessmachine/MainActivity.java
+++ app/src/main/java/com/example/uselessmachine/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.uselessmachine;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
@@ -11,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
 
     private Switch switchUseless;
     private Button buttonSelfDestruct;
+    
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +31,42 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if(isChecked){
-                    Toast.makeText(MainActivity.this, "On", Toast.LENGTH_SHORT).show();
+                    startSwitchOffTimer();
+                       //  Toast.makeText(MainActivity.this, "On", Toast.LENGTH_SHORT).show();
                 } else {
                     Toast.makeText(MainActivity.this, "Off", Toast.LENGTH_SHORT).show();
                 }
             }
         });
+    }
+
+    private void startSwitchOffTimer() {
+        new CountDownTimer(5000, 100){
+
+            /**
+             * Callback fired on regular interval.
+             *
+             * @param millisUntilFinished The amount of time until finished.
+             */
+            @Override
+            public void onTick(long millisUntilFinished) {
+                if(!switchUseless.isChecked()){
+                    //Log.d(TAG, "onTick: canceling");
+                    cancel();
+                }
+
+            }
+
+            /**
+             * Callback fired when the time is up.
+             */
+            @Override
+            public void onFinish() {
+                switchUseless.setChecked(false);
+                //Log.d(TAG, "onFinish: switch set to false");
+
+            }
+        }.start();
     }
 
     private void wireWidgets() {
